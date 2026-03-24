eb9f4673133106939e02b1165337f4c363807c68
pavandongare <dongare.pavan25@gmail.com>
2026-03-25 00:28:54 +0530

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,91 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [product,setProduct]=useState([]);
+  const [product, setProduct] = useState([]);
+  const [currPage, setCurrPage] = useState(0);
+  const [limit, setLimit] = useState(0);
+  useEffect(() => {
+    axios.get(`http://localhost:5001/api/products?page=${currPage}&limit=${limit}`).then((res) => {
+      res.data.respData && setProduct(res.data.respData);
+    })
+  }, [currPage, limit]); // [] 1st, ' ' every, var - var change
 
-  useEffect(()=>{
-    axios.get('http://localhost:5001/api/products').then(
-      (resp)=> {
-        return setProduct(resp.data.respData);
-      }
-    )
-  },[])
+
+  const onChangePage = (e) => {
+    setCurrPage(Number(e.target.value))
+  }
+
+  const onChangeLimit = (e) => {
+    setLimit(Number(e.target.value))
+  }
+
+  const Prev = () => {
+    if (currPage > 1) {
+      setCurrPage((val) => val - 1)
+    }
+
+  }
+  const Next = () => {
+  
+      setCurrPage((val) => val + 1)
+ 
+
+  }
+
+
   return (
     <div>
-      <pre>
-        {JSON.stringify(product,null,2)}
-      </pre>
+
+      {JSON.stringify(product, null, 2)}
+      {currPage}
+
+      <div className="flex justify-center bg-red-300 ">
+        <div className=" bg-gray-200 " >
+          {product.map((x) => {
+            return <div className=' bg-green-300 flex justify-center  m-5  w-20 rounded-full'>{x.name}</div>
+          })}
+        </div>
+
+      </div>
+
+      <div className="flex justify-center">
+        <div className="">
+          <div>
+            <input className="border-2 rounded-lg "
+              type='number'
+              value={currPage}
+              onChange={onChangePage}
+            ></input>
+          </div>
+
+          <button className="m-4 p-2 rounded-2" onClick={Prev}>Prev</button>
+        </div>
+
+
+
+        <div className="flex justify-center ">
+          <div className=" bg-green">
+            <div>
+              <input className="border-2 rounded-lg"
+                type='number'
+                value={limit}
+                onChange={onChangeLimit}
+              ></input>
+            </div>
+
+            <button className="m-4 p-2 rounded-2" onClick={Next}>next</button>
+          </div>
+
+        </div>
+
+
+
+
+      </div>
+
     </div>
-    )
+  )
 }
 
 export default App;
